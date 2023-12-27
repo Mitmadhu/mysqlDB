7f6e0edbe96d0e94d6f289d477a3414f8d1a2cad
Mitmadhu <65210584+Mitmadhu@users.noreply.github.com>
2023-12-27 18:03:06 +0530

    grpc test (#2)
    * addeded router for register and create dto for register
    
    * register user
    
    * grpc test added

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.21.1
 
 require (
 	github.com/Mitmadhu/commons v0.0.0-20231202054941-b52135c7f087
-	golang.org/x/crypto v0.13.0
+	golang.org/x/crypto v0.14.0
+	google.golang.org/grpc v1.60.1
+	google.golang.org/protobuf v1.32.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.4
 )
@@ -14,8 +16,13 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/Mitmadhu/mysqlDB/config"
 	"github.com/Mitmadhu/mysqlDB/database/model"
+	"github.com/Mitmadhu/mysqlDB/pb/grpc_test"
+	"github.com/Mitmadhu/mysqlDB/pb/grpc_user"
 	"github.com/Mitmadhu/mysqlDB/server"
+	"google.golang.org/grpc"
 	"gorm.io/gorm"
 )
 
@@ -33,7 +39,66 @@ func migrateTables() {
 
 func main() {
 	config.InitConfig()
-	server.InitRouter()
+	go func ()  {
+		server.InitRouter()
+	}()
+
+	go func ()  {
+		server.StartGRPCServer()
+	}()
+
+	time.Sleep(time.Second)
+	
+	
+
+	// ---------------------------
+	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("error in grpc dial", err.Error())
+	}
+	defer conn.Close()
+	client := grpc_user.NewMysqlClient(conn)
+	
+	request := grpc_user.ValidateUserRequest{
+		MsgID: "1230",
+		Password: "123",
+		Username: "123",
+		Token: "123",
+	}
+	resp, err := client.UserExists(context.Background(), &request)
+	if err != nil {
+		log.Fatal("error in login call", err.Error())
+	}
+
+	fmt.Printf("%+v\n", resp)
+
+	// ------------------------
+	// time.Sleep(time.Second)
+
+	testclient := grpc_test.NewTestClient(conn)
+	
+	req := grpc_test.TestRequest{
+		MsgID: "123",
+	}
+	respTest, err := testclient.SayHello(context.Background(), &req)
+	if err != nil {
+		log.Fatal("error in login call", err.Error())
+	}
+
+	fmt.Printf("%+v\n", respTest)
+
+
+
+	// _____
+
+	resp, err = client.UserExists(context.Background(), &request)
+	if err != nil {
+		log.Fatal("error in login call", err.Error())
+	}
+
+	fmt.Printf("%+v\n", resp)
+
+
 	// migrateTables()
 	
 
